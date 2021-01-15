28dcc1dc6dcf93abd0ab3f2c9404659a935bd420
shelcia <shelcia1999@gmail.com>
2021-01-15 22:20:59 +0530

    update

--- models/User.js
+++ models/User.js
@@ -1,1 +1,42 @@
-//
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 512,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024,
+  },
+  document: {
+    type: Array,
+    required: true,
+    min: 2,
+    max: 512,
+  },
+  token: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 1024,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
