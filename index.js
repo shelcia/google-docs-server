464abc07bd47ddd49cbee14c2a85be05b4f4646a
VMS-Backend <shelcia1999@gmail.com>
2021-07-21 00:58:29 +0530

    🔥 🔥 🔥

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 4050;
-const SOCKET_PORT = 4000;
+const PORT = 4000 || process.env.PORT;
 
-const io = require("socket.io")(SOCKET_PORT, {
+const io = require("socket.io")(PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
-
-//IMPORT ROUTES
-
-const authRoute = require("./routes/auth/auth");
-// const adminRoute = require("./routes/admin/admin");
-// const commonRoute = require("./routes/common/user");
 
 dotenv.config();
 
@@ -34,24 +26,13 @@ mongoose.connect(
   () => console.log("connected to db")
 );
 
-//MIDDLEWARE
+// app.get("/", (req, res) => {
+//   res.send(`<p>Hey! It's working</p>`);
+// });
 
-app.use(express.json(), cors());
-
-//ROUTE MIDDLEWARE
-
-app.use("/api/auth", authRoute);
-// app.use("/api/admin", adminRoute);
-// app.use("/api/common", commonRoute);
-
-app.get("/", (req, res) => {
-  res.send(`<p>Hey! It's working</p>`);
-});
-
-app.listen(PORT, () => console.log(`server up and running at  ${PORT}`));
+// app.listen(PORT, () => console.log(`server up and running at  ${PORT}`));
 
 io.on("connection", (socket) => {
-  // console.log(socket);
   socket.on("send-changes", (delta) => {
     console.log(delta);
     socket.broadcast.emit("receive-changes", delta);
