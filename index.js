618eefb8b3f8ea2cb8819a7a20ae0fff7e2dd4ee
VMS-Backend <shelcia1999@gmail.com>
2021-08-28 06:51:11 +0530

    🐛 🐛 🐛

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 4000 || process.env.PORT;
+const Document = require("./models/Document");
 
 const io = require("socket.io")(PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -26,15 +27,35 @@ mongoose.connect(
   () => console.log("connected to db")
 );
 
-// app.get("/", (req, res) => {
-//   res.send(`<p>Hey! It's working</p>`);
-// });
-
-// app.listen(PORT, () => console.log(`server up and running at  ${PORT}`));
-
 io.on("connection", (socket) => {
   socket.on("send-changes", (delta) => {
-    // console.log(delta);
+    console.log(delta.delta);
     socket.broadcast.emit("receive-changes", delta);
   });
 });
+
+// const defaultValue = "";
+
+// io.on("connection", (socket) => {
+//   socket.on("get-document", async (documentId) => {
+//     const document = await findOrCreateDocument(documentId);
+//     socket.join(documentId);
+//     socket.emit("load-document", document.data);
+
+//     socket.on("send-changes", (delta) => {
+//       socket.broadcast.to(documentId).emit("receive-changes", delta);
+//     });
+
+//     socket.on("save-document", async (data) => {
+//       await Document.findByIdAndUpdate(documentId, { data });
+//     });
+//   });
+// });
+
+// async function findOrCreateDocument(id) {
+//   if (id == null) return;
+
+//   const document = await Document.findById(id);
+//   if (document) return document;
+//   return await Document.create({ _id: id, data: defaultValue });
+// }
